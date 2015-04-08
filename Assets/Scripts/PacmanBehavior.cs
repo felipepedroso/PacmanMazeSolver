59d71fe0684a249ce19071cf25dcae78b2e54eb5
Felipe Pedroso <f.pedroso677@gmail.com>
2015-04-08 19:07:03 -0300

    Fixing some bugs and adding buttons to test DFS and BFS.
    Signed-off-by: Felipe Pedroso <f.pedroso677@gmail.com>

--- Assets/Scripts/PacmanBehavior.cs
+++ Assets/Scripts/PacmanBehavior.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class PacmanBehavior : MonoBehaviour {
 	public float Speed;
+
+
 	private Maze maze;
+
+	List<Int32Point> path;
+
+	GameObject dfsButtonGameObject;
+
+	UnityAction dfsUnityAction;
 
 	// Use this for initialization
 	void Start () {
@@ -16,20 +26,27 @@ public class PacmanBehavior : MonoBehaviour {
 		if (!LeanTween.isTweening (gameObject)) {
 			gameObject.GetComponent<Animator>().enabled = false;
 
-			List<Int32Point> path = null;
-
-			if (Input.GetKeyUp (KeyCode.Space)) {
-				path = maze.Graph.DepthFirstSearchPath (maze.PacmanLayer.GetTilePosition (gameObject));
+			if (Input.GetKeyUp (KeyCode.Q)) {
+				DoDFS();
 			}
-			if (Input.GetKeyUp (KeyCode.A)) {
-				path = maze.Graph.BreadthFirstSearchShortestPath (maze.PacmanLayer.GetTilePosition (gameObject));
+			if (Input.GetKeyUp (KeyCode.W)) {
+				DoBFS();
 			}
 
 			if (path != null && path.Count > 0) {
 				maze.PacmanLayer.MoveThroughPath (gameObject, path, Speed);
+				path = null;
 			}
 		} else {
 			gameObject.GetComponent<Animator>().enabled = true;
 		}
+	}
+
+	public void DoDFS(){
+		path = maze.Graph.DepthFirstSearchPath (maze.PacmanLayer.GetTilePosition (gameObject));
+	}
+
+	public void DoBFS(){
+		path = maze.Graph.BreadthFirstSearchShortestPath (maze.PacmanLayer.GetTilePosition (gameObject));
 	}
 }
---
+++ Assets/Scripts/SearchButtonsBehavior.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SearchButtonsBehavior : MonoBehaviour
+{
+	public void DoDfsClick ()
+	{
+		PacmanBehavior pacmanBehavior = GetPacmanBehavior ();
+		
+		if (pacmanBehavior != null) {
+			pacmanBehavior.DoDFS ();
+		}
+	}
+
+	public PacmanBehavior GetPacmanBehavior ()
+	{
+		GameObject pacmanGameObject = GameObject.Find ("PacmanTile");
+		PacmanBehavior pacmanBehavior = null;
+
+		if (pacmanGameObject != null) {
+			pacmanBehavior = pacmanGameObject.GetComponent<PacmanBehavior> ();
+		}
+		return pacmanBehavior;
+	}
+
+	public void DoBfsClick ()
+	{
+		PacmanBehavior pacmanBehavior = GetPacmanBehavior ();
+			
+		if (pacmanBehavior != null) {
+			pacmanBehavior.DoBFS ();
+		}
+	}
+}

cd3d0d29feb9313cd974122e64e682fd9b124fef
Felipe Pedroso <f.pedroso677@gmail.com>
2015-04-12 13:52:09 -0300

    Implementing Ghost State Machine.
    Signed-off-by: Felipe Pedroso <f.pedroso677@gmail.com>

--- Assets/Scripts/GhostBehavior.cs
+++ Assets/Scripts/GhostBehavior.cs
@@ -1,16 +1,116 @@
 ﻿using System.Collections;
 using System;
+using System.Text;
+using UnityEngine;
 
-public class GhostBehavior : MovableBehavior {
+public class GhostBehavior : MovableBehavior
+{
+	//public bool isPacmanNear;
+	//public bool isPacmanStronger;
+
+	public enum GhostState
+	{
+		Walking,
+		Chasing,
+		Evading
+	}
+
+	public GhostState CurrentState { get; private set; }
+
+	void Start ()
+	{
+		CurrentState = GhostState.Walking;
+	}
+
+	void Update ()
+	{
+		switch (CurrentState) {
+		case GhostState.Chasing:
+			Chase ();
+			break;
+		case GhostState.Evading:
+			Evade ();
+			break;
+		case GhostState.Walking:
+		default:
+			Walk ();
+			break;
+		}
+	}
+
+	void ChangeState (GhostState newState)
+	{
+		GhostState PreviousState = CurrentState;
+		CurrentState = newState;
+		Debug.Log (string.Format ("Switched from {0} to {1}", PreviousState, CurrentState));
+	}
+
 	private float deltaTime = 0;
-	void Update () {
+	private Direction randomDirection;
+
+	void Walk ()
+	{
+		if (IsPacmanNear ()) {
+			if (IsPacmanStronger ()) {
+				ChangeState (GhostState.Evading);
+				return;
+			} else {
+				ChangeState (GhostState.Chasing);
+				return;
+			}
+		}
+
+		// Walk logic
 		if (deltaTime > 0.2) {
 			deltaTime = 0;
 			Direction[] directions = (Direction[])Enum.GetValues (typeof(Direction));
-			Move (directions [UnityEngine.Random.Range (0, directions.Length)]);
+			randomDirection = directions [UnityEngine.Random.Range (0, directions.Length)];
+			Move (randomDirection);
 		} else {
 			deltaTime += UnityEngine.Time.deltaTime;
 		}
-
 	}
+
+	void Evade ()
+	{
+		if (!IsPacmanNear ()) {
+			ChangeState (GhostState.Walking);
+			return;
+		} else {
+			if (!IsPacmanStronger ()) {
+				ChangeState (GhostState.Chasing);
+				return;
+			}
+		}
+
+		// Evasion Logic
+	}
+
+	void Chase ()
+	{
+		if (!IsPacmanNear ()) {
+			ChangeState (GhostState.Walking);
+			return;
+		} else {
+			if (IsPacmanStronger ()) {
+				ChangeState (GhostState.Evading);
+				return;
+			}
+		}
+
+		// Chasing logic
+	}
+
+	bool IsPacmanNear ()
+	{
+		//return isPacmanNear;
+		return false;
+	}
+
+	bool IsPacmanStronger ()
+	{
+		//return isPacmanStronger;
+		return false;
+	}
+
 }
